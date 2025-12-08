795bd718cba908c27f0bdbf729b6229138f28d1f
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2025-12-08 12:25:25 -0500

    feat: automatically adjust number of columns for randomly generated npcs

--- TableHelper/wwwroot/TableHelper.lib.module.js
+++ TableHelper/wwwroot/TableHelper.lib.module.js
@@ -2,3 +2,17 @@ window.emitCustomEvent = (eventName) => {
     const myEvent = new CustomEvent(eventName);
     window.dispatchEvent(myEvent);
 };
+
+export const globalRegisterResizeHandler = (dotnetHelper) => {
+    function reportSize() {
+        let width = window.innerWidth;
+        try {
+            dotnetHelper.invokeMethodAsync('UpdateCols', width);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    window.addEventListener('resize', reportSize);
+    reportSize();
+}
